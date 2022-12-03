cee399975b5b2e482fcecd1dbd6600176a53802a
ovatimee <stonegee606@yahoo.com>
2022-12-03 08:26:46 +0000

    modified:   app.js modified:   controllers/authorController.js modified:   controllers/bookController.js modified:   controllers/bookInstanceController.js modified:   controllers/genreController.js modified:   models/author.js modified:   models/book.js modified:   models/bookInstance.js modified:   models/genre.js modified:   package-lock.json modified:   package.json modified:   public/stylesheets/style.css new file:   views/author_list.pug new file:   views/book_detail.pug new file:   views/book_list.pug new file:   views/bookinstance_list.pug deleted:    views/error.ejs new file:   views/error.pug new file:   views/genre_detail.pug new file:   views/genres.pug deleted:    views/index.ejs new file:   views/index.pug new file:   views/layout.pug

--- models/genre.js
+++ models/genre.js
@@ -6,4 +6,10 @@ const GenreSchema = new Schema({
   name: {type: String, minLength: 3, maxLength: 100}
 })
 
+
+GenreSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/genre/${this._id}`;
+});
+
 module.exports = mongoose.model("Genre", GenreSchema)
